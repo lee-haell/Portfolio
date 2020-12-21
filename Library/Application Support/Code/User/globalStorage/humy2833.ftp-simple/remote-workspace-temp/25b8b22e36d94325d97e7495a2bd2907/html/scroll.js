e64b31ec733e56e5c1fca0f981d1de6d0cb71137
lee-haell <haeeell@naver.com>
2020-12-21 21:24:26 +0900

    scrollNav 함수 생성

--- Library/Application Support/Code/User/globalStorage/humy2833.ftp-simple/remote-workspace-temp/25b8b22e36d94325d97e7495a2bd2907/html/scroll.js
+++ Library/Application Support/Code/User/globalStorage/humy2833.ftp-simple/remote-workspace-temp/25b8b22e36d94325d97e7495a2bd2907/html/scroll.js
@@ -1,5 +1,27 @@
 const SCROLL = "scroll";
 
+function scrollNav(event){
+    event.preventDefault();
+    
+    const li = document.querySelectorAll('header nav ul li'),
+        con4 = document.querySelector('.item4_wrap'),
+        footer = document.querySelector('footer'),
+        footerOffset = footer.offsetTop / 1.1,
+        conOffset4 = con4.offsetTop,
+        screenOffset = window.pageYOffset;
+
+    for(i=0; i<li.length; i++){
+        if(conOffset4 < screenOffset){
+            li[i].classList.add(SCROLL);
+        } else {
+            li[i].classList.remove(SCROLL);
+        }
+        if(footerOffset < screenOffset){
+            li[i].classList.remove(SCROLL);
+        }
+    }
+}
+
 function scrollTitle(){
     const con1 = document.querySelector('.item1_content'),
         con2 = document.querySelector('.item2_content'),
@@ -23,14 +45,15 @@ function moveText(){
     const item4Wrap = document.querySelector('.item4_wrap'),
         item4Text = item4Wrap.querySelector('.item4_moveText'),
         item4Position = item4Text.getBoundingClientRect().top,
-        screenPosition2 = window.innerHeight / 0.6;
+        screenPosition = window.innerHeight / 0.6;
 
-    if(item4Position < screenPosition2){
+    if(item4Position < screenPosition){
         item4Text.classList.add(SCROLL);
     } else {
         item4Text.classList.remove(SCROLL);
     }
 }
 
+window.addEventListener('scroll', scrollNav);
 window.addEventListener('scroll', scrollTitle);
 window.addEventListener('scroll', moveText);
